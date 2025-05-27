5a3e03aa2360f7771678cd8c57b251a6dafd7130
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-05-27 17:32:09 -0300

    chore: initialize project with package.json and dependencies

---
+++ index.js
@@ -0,0 +1,28 @@
+// Adicione "type": "module" no seu package.json
+// Exemplo:
+// {
+//   "name": "meu-projeto",
+//   "version": "1.0.0",
+//   "type": "module", // <-- Adicione esta linha
+//   "main": "index.js",
+//   "scripts": {
+//     "start": "node index.js"
+//   }
+// }
+
+import 'dotenv/config'; // <-- Mude esta linha para carregar o .env
+import { GoogleGenAI } from "@google/genai";
+
+const apiKey = process.env.GOOGLE_API_KEY;
+
+const ai = new GoogleGenAI({ apiKey: apiKey });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "bom diaa",
+  });
+  console.log(response.text);
+}
+
+await main();
